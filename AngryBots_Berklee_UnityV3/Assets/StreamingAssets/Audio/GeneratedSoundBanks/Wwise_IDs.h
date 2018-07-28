751f7a8fe224a83796b3ff71c6933e22103c86be
Abhi Acharya <abhi.acharya94@gmail.com>
2018-07-28 19:33:15 -0400

    rain ambience

---
+++ AngryBots_Berklee_UnityV3/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,178 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AIRLOCK_CLOSE = 702608323U;
+        static const AkUniqueID AIRLOCK_OPEN = 839967285U;
+        static const AkUniqueID AIRLOCK_TRANSITION_1_START = 3791349525U;
+        static const AkUniqueID AIRLOCK_TRANSITION_1_STOP = 3100208119U;
+        static const AkUniqueID AMBIENCE_FINAL_ROOM_START = 1369625819U;
+        static const AkUniqueID AMBIENCE_FINAL_ROOM_STOP = 2042180025U;
+        static const AkUniqueID AMBIENCE_INTERIOR_1_START = 1814667637U;
+        static const AkUniqueID AMBIENCE_INTERIOR_1_STOP = 3885289431U;
+        static const AkUniqueID AMBIENCE_INTERIOR_2_START = 2984579702U;
+        static const AkUniqueID AMBIENCE_INTERIOR_2_STOP = 3887226870U;
+        static const AkUniqueID AMBIENCE_INTERIOR_3_STOP = 1095387561U;
+        static const AkUniqueID AMBIENCE_INTERIOR_4_START = 1442938524U;
+        static const AkUniqueID AMBIENCE_INTERIOR_4_STOP = 3355271280U;
+        static const AkUniqueID AMBIENCE_RAIN_1_START = 695963679U;
+        static const AkUniqueID AMBIENCE_RAIN_1_STOP = 375445989U;
+        static const AkUniqueID AMBIENCE_RAIN_2_START = 1784184548U;
+        static const AkUniqueID AMBIENCE_RAIN_2_STOP = 3606242328U;
+        static const AkUniqueID AMBIENCE_RAIN_3_START = 3001268697U;
+        static const AkUniqueID AMBIENCE_RAIN_3_STOP = 3047115187U;
+        static const AkUniqueID AMBIENCE_RAIN_4_START = 1221095134U;
+        static const AkUniqueID AMBIENCE_RAIN_4_STOP = 4132975230U;
+        static const AkUniqueID AMBIENCE_RAIN_5_START = 3137340179U;
+        static const AkUniqueID AMBIENCE_RAIN_5_STOP = 2779608273U;
+        static const AkUniqueID AMBIENCE_RAIN_6_START = 1572935752U;
+        static const AkUniqueID AMBIENCE_RAIN_6_STOP = 1223273364U;
+        static const AkUniqueID BULLET_METAL = 1963562817U;
+        static const AkUniqueID BULLET_METAL_ENEMY = 2760029448U;
+        static const AkUniqueID BUZZERBOT_EXPLO = 1854098149U;
+        static const AkUniqueID BUZZERBOT_FLYING = 981100464U;
+        static const AkUniqueID BUZZERBOT_FLYING_STOP = 1117468863U;
+        static const AkUniqueID BUZZERBOT_WEAPONFIRE = 431158397U;
+        static const AkUniqueID CONVEYORSTART = 968321154U;
+        static const AkUniqueID CONVEYORSTOP = 2614491898U;
+        static const AkUniqueID DOOR_CLOSE = 128625444U;
+        static const AkUniqueID DOOR_OPEN = 535830432U;
+        static const AkUniqueID ELEVATOR_START = 1208976610U;
+        static const AkUniqueID ELEVATOR_STOP = 2818913498U;
+        static const AkUniqueID ENV_AIRLOCK_OFF = 4089091176U;
+        static const AkUniqueID ENV_AIRLOCK_ON = 1070816370U;
+        static const AkUniqueID EXTERIOR_STATE_EVENT = 551737296U;
+        static const AkUniqueID FAN = 1183803264U;
+        static const AkUniqueID FINAL_CUTSCENE_AUDIO = 3309699677U;
+        static const AkUniqueID FINAL_DOOR_OPEN = 2218572169U;
+        static const AkUniqueID FINAL_ROOM_MUSIC = 1017520215U;
+        static const AkUniqueID HACK_TERMINAL_PAUSE = 4060235250U;
+        static const AkUniqueID HACK_TERMINAL_RESUME = 1761116325U;
+        static const AkUniqueID HACK_TERMINAL_START = 3721549040U;
+        static const AkUniqueID HACK_TERMINAL_SUCCESS = 1431888493U;
+        static const AkUniqueID INTERIOR_STATE_EVENT = 3820973798U;
+        static const AkUniqueID LOCKED_DOOR_OPEN = 452833109U;
+        static const AkUniqueID MAIN_MUSIC = 63796330U;
+        static const AkUniqueID MECHBOT_ALERT = 3058437118U;
+        static const AkUniqueID MECHBOT_EXPLO = 2458149802U;
+        static const AkUniqueID MECHBOT_MISSILE_EXPLO = 3986069335U;
+        static const AkUniqueID MECHBOT_STEP = 712988602U;
+        static const AkUniqueID MECHBOT_WARNING_DIALOG = 3620036411U;
+        static const AkUniqueID MECHBOT_WEAPONFIRE = 990398604U;
+        static const AkUniqueID PLAYER_DAMAGE = 2074073782U;
+        static const AkUniqueID PLAYER_DEATH = 3083087645U;
+        static const AkUniqueID PLAYER_DOOR_UNLOCKED_DIALOG = 782210190U;
+        static const AkUniqueID PLAYER_FINAL_ROOM_DIALOG = 1630699566U;
+        static const AkUniqueID PLAYER_INTRO_DIALOG = 3788840656U;
+        static const AkUniqueID PLAYER_LOWHEALTH_START = 1272970714U;
+        static const AkUniqueID PLAYER_LOWHEALTH_STOP = 168349042U;
+        static const AkUniqueID PLAYER_MECH_DESTROYED_DIALOG = 3438805297U;
+        static const AkUniqueID PLAYER_STEP_CATWALK = 3669971645U;
+        static const AkUniqueID PLAYER_STEP_PANEL = 2535025448U;
+        static const AkUniqueID PLAYER_STEP_SOLIDMETAL = 2608227548U;
+        static const AkUniqueID PLAYER_STEP_TILE = 1050373178U;
+        static const AkUniqueID PLAYER_WEAPONFIRE_START = 3927759940U;
+        static const AkUniqueID PLAYER_WEAPONFIRE_STOP = 2302022904U;
+        static const AkUniqueID PLAYERENTERVO1 = 2364729054U;
+        static const AkUniqueID PLAYERENTERVO2 = 2364729053U;
+        static const AkUniqueID PLAYERENTERVO3 = 2364729052U;
+        static const AkUniqueID SPIDERBOT_EXPLO = 3484185570U;
+        static const AkUniqueID SPIDERBOT_INITIAL_ALERT = 3372300307U;
+        static const AkUniqueID SPIDERBOT_PROXIMITY = 1346476143U;
+        static const AkUniqueID SPIDERBOT_STEP = 2568842066U;
+        static const AkUniqueID UTEC = 1571565890U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_STATES
+        {
+            static const AkUniqueID GROUP = 1690668539U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CUTSCENE = 1182958561U;
+                static const AkUniqueID DOOR_1_UNLOCKED = 2736208515U;
+                static const AkUniqueID DOOR_2_UNLOCKED = 1431675934U;
+                static const AkUniqueID DOOR_3_UNLOCKED = 3666218477U;
+                static const AkUniqueID EXTERIOR1 = 2425836568U;
+                static const AkUniqueID INTERIOR1 = 3381565638U;
+                static const AkUniqueID LOWHEALTH = 1017222595U;
+                static const AkUniqueID NORMAL = 1160234136U;
+            } // namespace STATE
+        } // namespace MUSIC_STATES
+
+        namespace PLAYER_STATE
+        {
+            static const AkUniqueID GROUP = 4071417932U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+            } // namespace STATE
+        } // namespace PLAYER_STATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace LOWHEALTHSTINGER
+        {
+            static const AkUniqueID GROUP = 3082677351U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LOWHEALTHOFF = 4115339370U;
+                static const AkUniqueID LOWHEALTHON = 2586492748U;
+            } // namespace SWITCH
+        } // namespace LOWHEALTHSTINGER
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CRITICAL_DIALOGUE_VOLUME = 3513493796U;
+        static const AkUniqueID ENV_AIRLOCK = 4194431010U;
+        static const AkUniqueID ENV_INT = 3511380856U;
+        static const AkUniqueID PLAYER_HEALTH = 215992295U;
+        static const AkUniqueID PLAYER_POSITION = 2221031936U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID MECH_DEFEATED_STINGER = 3857742100U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID NEW_SOUNDBANK = 4072029455U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
